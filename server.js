9561617f382d06101162708dd8dcec5c80b2b6ff
HelenHMaher <helenharlanmaher@gmail.com>
2020-09-25 18:34:30 +0200

    server

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const favicon = require("express-favicon");
+const bodyParser = require("body-parser");
+const path = require("path");
+const sessionApi = require("./src/route/sessionApi");
+const app = express();
+const mongo = require("mongodb").MongoClient;
+
+mongo.connect(
+  process.env.MONGO_URI,
+  { useUnifiedTopology: true },
+  (err, client) => {
+    let db = client.db("my-daily-climb");
+    if (err) {
+      console.log("Database err: " + err);
+    } else {
+      console.group("Successful database connection");
+
+      app.use(bodyParser.json());
+      app.use(bodyParser.urlencoded({ extended: true }));
+
+      app.use(favicon(__dirname + "/build/favicon.png"));
+      app.get("/heartbeat", function (req, res) {
+        res.send("<3");
+      });
+
+      app.use("/", express.static(__dirname + "/build"));
+
+      app.get("/", function (req, res, next) {
+        res.sendFile(path.join(__dirname, "build", "index.html"));
+      });
+
+      sessionApi(app, db);
+
+      app.listen(process.env.PORT || 3000, () =>
+        console.log("Server is running...")
+      );
+    }
+  }
+);
