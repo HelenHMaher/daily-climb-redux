2dc5d6c09b02012aa179add218b0c76c3fcfc093
HelenHMaher <helenharlanmaher@gmail.com>
2020-09-28 12:00:13 +0200

    workoutTypesSlice

---
+++ src/components/workoutTypes/workoutTypesSlice.js
@@ -0,0 +1,34 @@
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+} from "@reduxjs/toolkit";
+import axios from "axios";
+
+const workoutTypesAdapter = createEntityAdapter();
+
+const initialState = workoutTypesAdapter.getInitialState();
+
+export const fetchWorkoutTypes = createAsyncThunk(
+  "workoutTypes/fetchWorkoutTypes",
+  async () => {
+    const response = await axios.get("/api/workoutTypes/");
+    return response.workoutTypes;
+  }
+);
+
+const workoutTypesSlice = createSlice({
+  name: "workoutTypes",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchWorkoutTypes.fulfilled]: workoutTypesAdapter.setAll,
+  },
+});
+
+export default workoutTypesSlice.reducer;
+
+export const {
+  selectAll: selectAllWorkoutTypes,
+  selectById: selectAllWorkoutTypesById,
+} = workoutTypesAdapter.getSelectors((state) => state.users);
--- src/components/workouts/AddWorkoutForm.js
+++ src/components/workouts/AddWorkoutForm.js
@@ -1,10 +1,51 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { unwrapResult } from "@reduxjs/toolkit";
+
 import { addNewWorkout } from "./workoutsSlice";
+import { selectAllWorkoutTypes } from "../workoutTypes/workoutTypesSlice";
 
 export const AddWorkoutForm = () => {
   const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [workoutTypeId, setWorkoutTypeId] = useState("");
+  const [addRequestStatus, setAddRequestStatus] = useState("idle");
+
+  const dispatch = useDispatch();
+  const workoutTypes = useSelector(selectAllWorkoutTypes);
 
   const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+  const onWorkoutTypeChanged = (e) => setWorkoutTypeId(e.target.value);
+
+  const canSave =
+    [title, content, workoutTypeId].every(Boolean) &&
+    addRequestStatus === "idle";
+
+  const onSaveWorkoutClicked = async () => {
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        const resultAction = await dispatch(
+          addNewWorkout({ title, content, workoutType: workoutTypeId })
+        );
+        unwrapResult(resultAction);
+        setTitle("");
+        setContent("");
+        setWorkoutTypeId("");
+      } catch (err) {
+        console.error("Failed to save post:", err);
+      } finally {
+        setAddRequestStatus("idle");
+      }
+    }
+  };
+
+  const workoutTypeOptions = workoutTypes.map((workoutType) => (
+    <option key={workoutType.id} value={workoutType.id}>
+      {workoutType.name}
+    </option>
+  ));
 
   return (
     <section>
@@ -18,6 +59,25 @@ export const AddWorkoutForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
+        <label htmlFor="workoutType">Workout Type"</label>
+        <select
+          id="workoutType"
+          value={workoutTypeId}
+          onChange={onWorkoutTypeChanged}
+        >
+          <option value=""></option>
+          {workoutTypeOptions}
+        </select>
+        <label htmlFor="workoutContent">Content:</label>
+        <textarea
+          id="workoutContent"
+          name="workoutContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button" onClick={onSaveWorkoutClicked} disable={!canSave}>
+          Save Workout
+        </button>
       </form>
     </section>
   );
