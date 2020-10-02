e6e2163517f12921535f179fc36f2922feae4078
HelenHMaher <helenharlanmaher@gmail.com>
2020-10-02 19:42:42 +0200

    success

--- src/components/workouts/WorkoutsList.js
+++ src/components/workouts/WorkoutsList.js
@@ -2,16 +2,19 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { selectAllWorkouts } from "./workoutsSlice";
-import { selectWorkoutTypeById } from "../workoutTypes/workoutTypesSlice";
+import { selectAllWorkoutTypes } from "../workoutTypes/workoutTypesSlice";
 
 export const WorkoutsList = () => {
   const workouts = useSelector(selectAllWorkouts);
   console.log(workouts);
 
+  const workoutTypes = useSelector(selectAllWorkoutTypes);
+
   const renderedWorkouts = workouts.undefined.map((workout) => {
-    const workoutType = useSelector((state) =>
-      selectWorkoutTypeById(state, workout.id)
+    const workoutType = workoutTypes.undefined.find(
+      (type) => type.id === workout.type
     );
+
     return (
       <li key={workout.id}>
         <Link to={`/workouts/${workout.id}`}>{workout.name}</Link>
