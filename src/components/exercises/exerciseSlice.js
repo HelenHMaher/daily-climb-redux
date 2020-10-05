7d981735ab8897b4b85a029a70f02f8dab962411
HelenHMaher <helenharlanmaher@gmail.com>
2020-10-05 22:31:10 +0200

    exerciseSlice

---
+++ src/components/exercises/exerciseSlice.js
@@ -0,0 +1,95 @@
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+} from "@reduxjs/toolkit";
+import axios from "axios";
+
+const exercisesAdapter = createEntityAdapter();
+
+const initialState = exercisesAdapter.getInitialState({
+  status: "idle",
+  error: null,
+});
+
+export const addNewExercise = createAsyncThunk(
+  "exercises/addNewExercise",
+  async (initialExercise) => {
+    const response = await axios.put("/api/exercises/", {
+      exercise: initialExercise,
+    });
+    return response;
+  }
+);
+
+export const editExercise = createAsyncThunk(
+  "exercises/editExercises",
+  async (initialExercise) => {
+    const response = await axios.post(`/api/exercises/${initialExercise}`, {
+      exercise: initialExercise,
+    });
+    return response;
+  }
+);
+
+export const fetchExercises = createAsyncThunk(
+  "exercises/fetchExercises",
+  async () => {
+    const response = await axios.get("/api/exercises/");
+    return response;
+  }
+);
+
+const exerciseSlice = createSlice({
+  name: "exercises",
+  initialState,
+  reducers: {
+    exerciseAdded(state, action) {
+      state.entities.undefined.push(action.payload);
+    },
+    exerciseUpdated(state, action) {
+      const { id, name, type, description } = action.payload;
+      const existingExercise = state.entities.undefined.filter(
+        (x) => x["id"] === id
+      );
+      if (existingExercise) {
+        existingExercise[0]["name"] = name;
+        existingExercise[0]["description"] = description;
+        existingExercise[0]["type"] = type;
+      }
+    },
+    exerciseDeleted(state, action) {
+      const { id } = action.payload;
+      const array = state.entities.undefined;
+      const existingExercise = array.filter((x) => x["id"] === id);
+      if (existingExercise) {
+        const index = array.indexOf(existingExercise);
+        array.splice(index, 1);
+      }
+    },
+  },
+  extraReducers: {
+    [fetchExercises.fulfilled]: exercisesAdapter.setAll,
+  },
+});
+
+export const {
+  exerciseAdded,
+  exerciseUpdated,
+  exerciseDeleted,
+} = exerciseSlice.actions;
+
+export default exerciseSlice.reducer;
+
+export const selectAllExercises = (state) => state.exercises.entities;
+
+export const selectExerciseById = (state, exerciseId) =>
+  state.exercises.entities.undefined.find(
+    (exercise) => exercise.id === exerciseId
+  );
+
+export const selectExerciseByWorkoutType = (state, workoutTypeId) => {
+  state.exercises.entities.undefined.filter(
+    (exercise) => exercise.type === workoutTypeId
+  );
+};
