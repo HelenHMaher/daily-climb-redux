f24ad9fca1eb9b417198c81148a45a167e2ed5cf
HelenHMaher <helenharlanmaher@gmail.com>
2020-10-10 20:49:29 +0200

    add exercise

--- src/components/workouts/AddExerciseEntry.js
+++ src/components/workouts/AddExerciseEntry.js
@@ -12,3 +12,34 @@
 //--maybe this page includes a counter for manually counting out sets?
 
 //there is a page for editing the details of existing exercise entries (you can edit all feilds and it won't effect the default)
+
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { exerciseAdded } from "./workoutsSlice";
+
+export const AddExerciseEntry = ({ workout, exercise }) => {
+  const dispatch = useDispatch();
+  const [setArray, setSetArray] = useState([]);
+  const [currentSetReps, setCurrentSetReps] = useState("");
+  const [currentSetWeight, setCurrentSetWeight] = useState("");
+  const [notes, setNotes] = useState("");
+
+  const onCurrentSetRepsChanged = (e) => setCurrentSetReps(e.target.value);
+  const onCurrentSetWeightChanged = (e) => setCurrentSetWeight(e.target.value);
+  const onNotesChanged = (e) => setNotes(e.target.value);
+
+  const onSaveSetClicked = () => {
+    const newSet = { reps: currentSetReps, weight: currentSetWeight };
+    setSetArray(setArray.push(newSet));
+  };
+
+  const onSaveExerciseClicked = () => {
+    dispatch(exerciseAdded({ sets: setArray, notes }));
+  };
+
+  return (
+    <section>
+      <h2>Add New Exercise</h2>
+    </section>
+  );
+};
