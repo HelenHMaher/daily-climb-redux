860b00893a1e271286b672c94cee6b2441d195e7
HelenHMaher <helenharlanmaher@gmail.com>
2020-10-21 23:59:45 +0200

    notes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
-
 import BaseApp from "./BaseApp";
+
+//this is where login information resolves
+//one important thing is how to keep track of if validated or not, if the user has been inactive: maybe set up a timer??
+//this is where the userapi is used
 
 function App() {
   return <BaseApp />;
