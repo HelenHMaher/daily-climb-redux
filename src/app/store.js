e8a19bb2c9eb40bf1cdf0c8c016435c53e08f75c
HelenHMaher <helenharlanmaher@gmail.com>
2020-10-21 19:14:22 +0200

    userSlice

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import workoutsReducer from "../components/workouts/workoutsSlice";
 import workoutTypesReducer from "../components/workoutTypes/workoutTypesSlice";
 import exercisesReducer from "../components/exercises/exerciseSlice";
+import userReducer from "../components/user/userSlice";
 
 export default configureStore({
   reducer: {
     workouts: workoutsReducer,
     workoutTypes: workoutTypesReducer,
     exercises: exercisesReducer,
+    user: userReducer,
   },
 });
---
+++ src/components/user/userSlice.js
@@ -0,0 +1,37 @@
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+} from "@reduxjs/toolkit";
+import axios from "axios";
+
+const userAdapter = createEntityAdapter();
+
+const initialState = userAdapter.getInitialState({
+  status: "idle",
+  error: null,
+});
+
+export const fetchUser = createAsyncThunk("user/fetchUser", async () => {
+  const response = await axios.get("/api/user/");
+  return response;
+});
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    userUpdated(state, action) {
+      const { username, email, password } = action.payload;
+      const existingUser = state.entities;
+      existingUser["username"] = username;
+      existingUser["password"] = password;
+      existingUser["email"] = email;
+    },
+    extraReducers: {
+      [fetchUser.fulfilled]: userAdapter.setAll,
+    },
+  },
+});
+
+export const { userUpdated } = userSlice.actions;
